e8e9bc9926a2e39e20168530a61cd9e0550b217c
Animesh-roy100 <royanimesh7478@gmail.com>
2024-06-14 18:11:00 +0530

    public params added

--- OOPs-Basic/main.cpp
+++ OOPs-Basic/main.cpp
@@ -2,15 +2,27 @@
 
 class Hero {
     // properties
+    public:
     char name[100];
     int health;
     char level;
+
+    void print() {
+        std::cout<<level<<std::endl;
+    }
 };
 
 int main() {
     // std::cout<<"Hello World!";
     // creation of object/instance
     Hero h1;
-    std::cout <<"size : " << sizeof(h1) << std::endl;
+    // std::cout <<"size : " << sizeof(h1) << std::endl;
+
+    h1.health = 20;
+    h1.level = 'A';
+
+    std::cout<< "health is: " << h1.health << std::endl;
+    std::cout<< "level is: " << h1.level << std::endl;
+
     return 0;
 }
