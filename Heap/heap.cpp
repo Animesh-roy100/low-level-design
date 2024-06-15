6b2839055a4d0709a95338600aa9e48a978e41ec
Animesh-roy100 <royanimesh7478@gmail.com>
2024-06-15 16:15:47 +0530

    heapify and heapSort added

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -11,6 +11,13 @@ class heap {
         size = 0;
     }
 
+    void print () {
+        for (int i=1; i<=size; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
     void insert(int val) {
         size = size+1;
         int index = size;
@@ -27,17 +34,67 @@ class heap {
         }
     }
 
-    void print () {
-        for (int i=1; i<=size; i++) {
-            cout << arr[i] << " ";
+    void deleteFromHeap() {
+        if(size == 0) {
+            cout<<"nothing to delete"<<endl;
+            return;
         }
-        cout << endl;
+
+        // Put last element into first index
+        arr[1] = arr[size];
+
+        // Remove the last element
+        size--;
+
+        // take root node to its correct position
+        int i=1;
+        while(i<size) {
+            int leftIndex = i*2;
+            int rightIndex = i*2+1;
+            if(leftIndex<size && arr[leftIndex] > arr[i]) {
+                swap(arr[leftIndex], arr[i]);
+                i=leftIndex;
+            } else if(rightIndex<size && arr[rightIndex] > arr[i]) {
+                swap(arr[rightIndex], arr[i]);
+                i=rightIndex;
+            } else {
+                return;
+            }
+        }
     }
 };
 
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2*i;
+    int right = 2*i+1;
+
+    if(left<=n && arr[largest] < arr[left]) {
+        largest = left;
+    }
+    if(right<=n && arr[largest] < arr[right]) {
+        largest = right;
+    }
+
+    if(largest != i) {
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n) {
+    int size=n;
+    while(size>1) {
+        swap(arr[size], arr[1]);
+        size--;
+
+        heapify(arr, size, 1);
+    }
+}
+
 int main() {
     heap h;
-    
+
     h.insert(50);
     h.insert(55);
     h.insert(53);
@@ -45,6 +102,29 @@ int main() {
     h.insert(54);
 
     h.print();
+
+    h.deleteFromHeap();
+    h.print();
+
+    int arr[6] = {-1, 54, 53, 55, 52, 50};
+    int n = 5;
+
+    // heap creation
+    for(int i=n/2; i>0; i--) {
+        heapify(arr, n, i);
+    }
+
+    cout<<"printing the array now: "<<endl;
+    for(int i=1; i<=n; i++) {
+        cout<<arr[i]<<" ";
+    } cout<<endl;
+
+    // heap sort
+    heapSort(arr, n);
+    cout<<"after sorting, the array now: "<<endl;
+    for(int i=1; i<=n; i++) {
+        cout<<arr[i]<<" ";
+    } cout<<endl;
 
     return 0;
 }
