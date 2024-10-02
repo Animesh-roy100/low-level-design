1a99f30922106e5f0da6a98f361b211f636eb304
Animesh-roy100 <royanimesh7478@gmail.com>
2024-10-03 03:00:03 +0530

    method overriding added

---
+++ oopsJava/methodOverrinding/Dog.java
@@ -0,0 +1,7 @@
+package oopsJava.methodOverrinding;
+
+public class Dog extends Animal {
+    public void makeSound() {
+        System.out.println("Wooof!!");
+    }
+}
---
+++ oopsJava/methodOverrinding/Main.java
@@ -0,0 +1,11 @@
+package oopsJava.methodOverrinding;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.makeSound();
+
+        Dog dog = new Dog();
+        dog.makeSound();
+    }    
+}
