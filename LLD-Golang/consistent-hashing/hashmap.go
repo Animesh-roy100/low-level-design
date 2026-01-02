f7484e14c2cb3e53aef769cecb7c9339783d927f
Animesh-roy100 <royanimesh7478@gmail.com>
2026-01-03 01:05:54 +0530

    consistent hashing example added

---
+++ LLD-Golang/consistent-hashing/hashmap.go
@@ -0,0 +1,105 @@
+package main
+
+/*
+
+ConsistentHashMap represents a consistent hashing ring.
+It contains: hashring array, number of replicas and hash function.
+
+Methods:
+- AddServer: adds a new server to the hash ring.
+- RemoveServer: removes a server from the hash ring.
+- GetServer: returns the server responsible for the given key.
+
+*/
+
+type ConsistentHashMap struct {
+	hashring map[uint32]*Server // sorted set
+	replicas int                // to reduce the possibility of domino effect
+	hashfunc IHash
+}
+
+// NewConsistentHashMap creates a new ConsistentHashMap.
+func NewConsistentHashMap(replicas int, hashfunc IHash) *ConsistentHashMap {
+	return &ConsistentHashMap{
+		hashring: make(map[uint32]*Server),
+		replicas: replicas,
+		hashfunc: hashfunc,
+	}
+}
+
+// wrte getter setter for all params
+func (c *ConsistentHashMap) GetHashRing() map[uint32]*Server {
+	return c.hashring
+}
+
+func (c *ConsistentHashMap) SetHashRing(hashring map[uint32]*Server) {
+	c.hashring = hashring
+}
+
+func (c *ConsistentHashMap) SetInHashRing(key uint32, server *Server) {
+	c.hashring[key] = server
+}
+
+func (c *ConsistentHashMap) GetFromHashRing(key uint32) *Server {
+	return c.hashring[key]
+}
+
+func (c *ConsistentHashMap) GetReplicas() int {
+	return c.replicas
+}
+
+func (c *ConsistentHashMap) SetReplicas(replicas int) {
+	c.replicas = replicas
+}
+
+func (c *ConsistentHashMap) GetHashFunc() IHash {
+	return c.hashfunc
+}
+
+func (c *ConsistentHashMap) SetHashFunc(hashfunc IHash) {
+	c.hashfunc = hashfunc
+}
+
+// AddServer adds a new server to the hash ring.
+func (c *ConsistentHashMap) AddServer(server *Server) {
+	for i := 0; i < c.replicas; i++ {
+		hash := c.GetHashFunc().Hash(server.GetIP() + string(rune(i)))
+		c.hashring[hash] = server
+	}
+}
+
+// RemoveServer removes a server from the hash ring.
+func (c *ConsistentHashMap) RemoveServer(server *Server) {
+	for i := 0; i < c.replicas; i++ {
+		hash := c.GetHashFunc().Hash(server.GetIP() + string(rune(i)))
+		serverToeDeleted := c.GetFromHashRing(hash)
+
+		// reindex all keys after the removed server
+		for _, entry := range serverToeDeleted.GetEntries() {
+			c.put(entry.GetKey(), entry.GetValue())
+		}
+	}
+}
+
+// todo:  this func  can be modified to (n log n) considering hash ring to be sorted map/ sorted set
+// use binary search
+// considering using this lib: https://github.com/umpc/go-sortedmap once i go through this, will integrate the same.
+func (c *ConsistentHashMap) GetServer(key string) *Server {
+	hash := c.GetHashFunc().Hash(key)
+	for k, v := range c.hashring {
+		if k >= hash {
+			return v
+		}
+	}
+	return c.hashring[0]
+}
+
+func (c *ConsistentHashMap) put(key string, value string) {
+	server := c.GetServer(key)
+	server.AddEntry(key, value)
+}
+
+func (c *ConsistentHashMap) get(key string) (string, error) {
+	server := c.GetServer(key)
+	return server.GetEntry(key)
+}
---
+++ LLD-Golang/consistent-hashing/server.go
@@ -0,0 +1,52 @@
+package main
+
+import "errors"
+
+/*
+
+Server represents a server in the consistent hashing ring.
+It contains: Server IP and list of key-value entries stored in the server.
+
+*/
+
+type Server struct {
+	ip      string
+	entries []*KeyVal
+}
+
+// NewServer creates a new Server with the given ip.
+func NewServer(ip string) *Server {
+	return &Server{
+		ip: ip,
+	}
+}
+
+// write all getter and setter for server
+func (s *Server) GetIP() string {
+	return s.ip
+}
+
+func (s *Server) SetIP(ip string) {
+	s.ip = ip
+}
+
+func (s *Server) GetEntries() []*KeyVal {
+	return s.entries
+}
+
+func (s *Server) SetEntries(entries []*KeyVal) {
+	s.entries = entries
+}
+
+func (s *Server) GetEntry(key string) (string, error) {
+	for _, entry := range s.entries {
+		if entry.GetKey() == key {
+			return entry.GetValue(), nil
+		}
+	}
+	return "", errors.New("key not found")
+}
+
+func (s *Server) AddEntry(key string, value string) {
+	s.entries = append(s.entries, NewKeyVal(key, value))
+}
